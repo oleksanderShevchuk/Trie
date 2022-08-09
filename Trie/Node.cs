30a8fe9bab78dd56e72f4366f29fa2be25f068a9
oleksandr.shevchuk222@gmail.com <oleksandr.shevchuk222@gmail.com>
2022-08-09 14:39:08 +0300

    Add project files.

---
+++ Trie/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trie
+{
+    public class Node<T>
+    {
+        public char Symbol { get; set; }
+        public T Data { get; set; }
+        public bool IsWord { get; set; }
+        public string Prefix { get; set; }
+
+        public Dictionary<char, Node<T>> SubNodes { get; set; }
+        public Node(char symbol, T data, string prefix)
+        {
+            Symbol = symbol;
+            Data = data;
+            Prefix = prefix;
+            SubNodes = new Dictionary<char, Node<T>>();
+        }
+        public Node<T> TryFind(char symbol)
+        {
+            if (SubNodes.TryGetValue(symbol, out Node<T> value))
+            {
+                return value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Data} [{SubNodes.Count}] ({Prefix})";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.Equals(item);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Trie/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Trie
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var trie = new Trie<int>();
+            trie.Add("привет", 50);
+            trie.Add("мир", 100);
+            trie.Add("приз", 200);
+            trie.Add("мирний", 100);
+            trie.Add("подарок", 50);
+            trie.Add("проект", 200);
+            trie.Add("прапорщик", 50);
+            trie.Add("правий", 100);
+            trie.Add("год", 150);
+            trie.Add("герой", 100);
+            trie.Add("голуб", 200);
+
+            trie.Remove("правий");
+            trie.Remove("мир");
+
+            Search(trie, "привет");
+            Search(trie, "мир");
+            Search(trie, "облоко");
+            Search(trie, "мирний");
+            Search(trie, "проект");
+            Search(trie, "год");
+        }
+        private static void Search(Trie<int> trie, string word)
+        {
+            if (trie.TrySearch(word, out int value))
+            {
+                Console.WriteLine(word + " " + value);
+            }
+            else
+                Console.WriteLine("Не знайдено " + word);
+        }
+    }
+}
